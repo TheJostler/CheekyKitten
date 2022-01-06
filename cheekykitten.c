629d9396ec06d33ea70e6679ba298086781f169f
TheJostler <65976463+TheJostler@users.noreply.github.com>
2022-01-06 16:15:49 +0000

    Update cheekykitten.c

--- cheekykitten.c
+++ cheekykitten.c
@@ -9,7 +9,7 @@ int main (int argc, char **argv) {
     
     char version[] = "CheekyKitten Beta0.2 by Josjuar Lister 2021-2022\n";
     char usage[] = "%s\n\n%s [options] <input file> <output file>\n"
-        "CheekyKitten will default to stdout/stdin if i/o files are provided\n\n"
+        "CheekyKitten will default to stdout/stdin if i/o files are not provided\n\n"
         "\t-h           Print this help menu\n"
         "\t-k <key>     RESERVED FOR FUTURE IMPLEMENTATION\n"
         "\t-b           Output as binary\n";
