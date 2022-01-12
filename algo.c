2fa7156aa8e79922829c31bf9d4ba687006c5013
TheJostler <65976463+TheJostler@users.noreply.github.com>
2022-01-12 22:05:52 +0000

    Update algo.c

--- algo.c
+++ algo.c
@@ -5,27 +5,21 @@
  */
 
 //IXI
-int x_ixi(int x, int y) {
-    int d = y% 0x10;
+int x_ixi(int x, int y, int d) {
     x = (y - d) + (x / 0x10);
 	return x;
 	}
-int y_ixi(int x, int y) {
-    int b = x% 0x10;
-    int d = y% 0x10;
-    y = (d * 0x10) + b;
+int y_ixi(int b, int d) {
+    int y = (d * 0x10) + b;
 	return y;
 }
 
 //XX
-int x_xx(int x, int y) {
-    int b = x% 0x10;
-    int d = y% 0x10;
-	x = b * 0x10 + d;
+int x_xx(int b, int d) {
+	int x = b * 0x10 + d;
 	return x;
 	}
-int y_xx(int x, int y) {
-    int b = x% 0x10;
+int y_xx(int x, int y, int b) {
 	y = (x - b) + (y / 0x10);
 	return y;
 }
