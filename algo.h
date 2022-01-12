faff436841918d256fd3642a4e84ae793f511304
TheJostler <65976463+TheJostler@users.noreply.github.com>
2022-01-12 22:07:52 +0000

    Update algo.h

--- algo.h
+++ algo.h
@@ -1,10 +1,10 @@
-#ifndef QC_H
-#define QC_H
+#ifndef ALGO_H
+#define ALGO_H
 
-int x_ixi(int x, int y);
-int y_ixi(int x, int y);
+int x_ixi(int x, int y, int d);
+int y_ixi(int b, int d);
 
-int x_xx(int x, int y);
-int y_xx(int x, int y);
+int x_xx(int b, int d);
+int y_xx(int x, int y, int b);
 
 #endif
