1ffff88992ca3e061ed678b8982d310d15473e96
TheJostler <josj@wjaag.com>
2022-01-13 20:38:17 +0000

    .

--- TESTING/algo.c
+++ TESTING/algo.c
@@ -6,19 +6,15 @@
 
 //IXI
 int x_ixi(int x, int y) {
-    x = (0xf0 & y)^(x >> 4);
-	return x;
+    return (0xf0 & y)^((x & 0xf0) >> 4);
 	}
 int y_ixi(int x, int y) {
-    y = (y&0xf<<4)^(x&0xf);
-	return y;
+    return (y & 0xf << 4)^(x & 0xf);
 }
 //XX
 int x_xx(int x, int y) {
-    x = (x&0xf<<4)^(y&0xf);
-	return x;
+    return (x & 0xf << 4)^(y & 0xf);
 	}
 int y_xx(int x, int y) {
-    y = (0xf0 & x)^(y >> 4);
-	return y;
+    return (0xf0 & x)^(y >> 4);
 }
